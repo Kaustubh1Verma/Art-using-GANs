c29acb82d254f401a47398f274fa9db18e2a6d9c
Kaustubh Verma <33328916+Kaustubh1Verma@users.noreply.github.com>
2021-12-20 11:25:44 +0530

    Add files via upload

---
+++ MSG-GAN/config.py
@@ -0,0 +1,11 @@
+"""Global configuration."""
+
+# ----------------------------------------------------------------------------
+# Paths.
+
+result_dir = "config_dir/results/"
+data_dir = "/home/b16004/wd/MSG-GAN2/tfrecords/"
+cache_dir = "config_dir/cache/"
+run_dir_ignore = ["results", "datasets", "cache"]
+
+# ----------------------------------------------------------------------------
